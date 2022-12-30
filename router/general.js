6267b0ad962cb8dba5bea993fa35cc1989256c9a
Rano Kuhl <ranokuhl@gmail.com>
2022-12-30 11:30:58 +0100

    task 5 done

--- router/general.js
+++ router/general.js
@@ -49,8 +49,13 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-	//Write your code here
-	return res.status(300).json({message: 'Yet to be implemented'})
+	const reviewISBN = req.params.isbn
+
+	Object.entries(books).map(book => {
+		if (book[0] === reviewISBN) {
+			res.send(book[1].reviews)
+		}
+	})
 })
 
 module.exports.general = public_users
